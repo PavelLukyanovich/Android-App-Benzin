9a05d9469ed6b62ebcc6d1c50d1c365c2b3f12cc
Pavel Lukyanovich <pavellukyanovich1@gmail.com>
2022-07-06 17:15:25 +0300

    UI first activity and Date Picker

--- app/src/main/java/com/example/benzin/MainActivity.java
+++ app/src/main/java/com/example/benzin/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.benzin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView selectDateText;
+//    EditText selectDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectDateText = findViewById(R.id.selectDateText);
+//        selectDate = findViewById(R.id.selectDate);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        selectDateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                String date = day + "/" + month + "/" + year;
+                selectDateText.setText(date);
+            }
+        };
+//        selectDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+//                        new DatePickerDialog.OnDateSetListener() {
+//                            @Override
+//                            public void onDateSet(DatePicker view, int year, int month, int day) {
+//                                month = month + 1;
+//                                String date = day + "/" + month + "/" + year;
+//                                selectDate.setText(date);
+//                            }
+//                        }, year, month, day);
+//                datePickerDialog.show();
+//            }
+//
+//        });
     }
 }
+
